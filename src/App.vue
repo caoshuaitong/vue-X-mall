fe41d0fd640d67d9bd1657bd664d137aebd33af9
tan <718869429@qq.com>
2020-01-19 16:28:42 +0800

    Start

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+  filters: {},
+  watch: {},
+  computed: {}
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+//引入dayjs
+import dayjs from 'dayjs'
+//引入axios
+import axios from './http'
+//引入view
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+//引入接口文档
+import api from './http/api'
+Vue.prototype.$api = api
+
+Vue.config.productionTip = false
+Vue.prototype.$dayjs = dayjs
+Vue.prototype.$axios = axios
+Vue.use(ViewUI);
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
