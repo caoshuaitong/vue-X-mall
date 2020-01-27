e219f2f274d9424a02c64fe61113e1877e5301a1
Trayvon21 <718869429@qq.com>
2020-01-27 19:09:29 +0800

    完成轮播特效

--- src/components/index/XmCarousel.vue
+++ src/components/index/XmCarousel.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <Carousel v-model="value" height="500px" loop>
-      <CarouselItem v-for="item in panelContents" :key="item.id">
-        <img class="picUrl" :src="item.picUrl" alt />
-        <img class="picUrl2" :src="item.picUrl2" alt />
-        <img class="picUrl3" :src="item.picUrl3" alt />
-      </CarouselItem>
-    </Carousel>
+  <div class="box">
+    <div
+      @mouseover="Over($refs.carousel)"
+      @mousemove="Move($refs.carousel,$event)"
+      @mouseout="Out($refs.carousel)"
+      ref="carousel"
+      class="carousel"
+    >
+      <Carousel v-model="value" :height="500" loop radius-dot>
+        <CarouselItem v-for="item in panelContents" :key="item.id">
+          <img class="picUrl" :src="item.picUrl" alt />
+          <img class="picUrl2" :src="item.picUrl2" alt />
+          <img class="picUrl3" :src="item.picUrl3" alt />
+        </CarouselItem>
+      </Carousel>
+    </div>
   </div>
 </template>
 
@@ -17,7 +25,13 @@ export default {
   data() {
     return {
       panelContents: [],
-      value: 0
+      value: 0,
+      option: {
+        offsetLeft: 0,
+        offsetTop: 0,
+        offsetWidth: 0,
+        offsetHeight: 0
+      }
     };
   },
   computed: {},
@@ -34,6 +48,28 @@ export default {
           console.log(this.panelContents);
         }
       });
+    },
+    //鼠标移入获取当前dom的状态
+    Over(dom) {
+      this.option = {
+        offsetLeft: dom.offsetLeft,
+        offsetTop: dom.offsetTop,
+        offsetWidth: dom.offsetWidth,
+        offsetHeight: dom.offsetHeight
+      };
+    },
+    //操作dom元素
+    Move(dom, eve) {
+      let X, Y;
+      let mouseX = eve.pageX - this.option.offsetLeft;
+      let mouseY = eve.pageY - this.option.offsetTop;
+      X = mouseX - this.option.offsetWidth / 2;
+      Y = this.option.offsetHeight / 2 - mouseY;
+      dom.style.transform = `rotateY(${X / 50}deg) rotateX(${Y / 50}deg)`;
+    },
+    //鼠标移出还原dom元素状态
+    Out(dom) {
+      dom.style.transform = "rotateY(0deg) rotateX(0deg)";
     }
   },
   components: {}
@@ -41,13 +77,38 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.box {
+  perspective: 1000px;
+}
+.carousel {
+  position: relative;
+  width: 1220px;
+  height: 500px;
+  margin: 20px auto;
+  background-size: 100% 100%;
+  border-radius: 10px;
+  transform-origin: 50% 50%;
+  transform: rotateY(0deg) rotateX(0deg);
+  transition: all 0.5s linear;
+}
 .ivu-carousel {
   margin: 20px auto;
   width: 1220px;
   height: 500px;
 }
-
+/deep/.ivu-carousel-arrow,
+/deep/.ivu-carousel-arrow:hover {
+  display: none !important;
+}
+/deep/.ivu-carousel-dots {
+  .radius {
+    width: 15px !important;
+    height: 15px !important;
+    margin-top: -20px;
+  }
+}
 .ivu-carousel-item {
+  z-index: 999;
   position: relative;
   .picUrl {
     position: absolute;
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="xm-center">
+  <div>
     <xm-carousel />
   </div>
 </template>
